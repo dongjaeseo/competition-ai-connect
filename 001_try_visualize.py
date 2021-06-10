442d7a37edf5d15d13c7d361609d956239507dd2
DongJae Seo <75518803+dongjaeseo@users.noreply.github.com>
2021-06-10 17:55:07 +0900

    visualization of sample images

--- 001_try_visualize.py
+++ 001_try_visualize.py
@@ -2,18 +2,34 @@ import cv2
 import json
 import ast
 import matplotlib.pyplot as plt
+from PIL import Image
+from PIL import ImageDraw
 
-# img = cv2.imread('./sample_hairstyle/sample_image/img01.jpg')
-# cv2.imshow('img', img)
-# cv2.waitKey(0)
+for i in range(10):
+    img = Image.open(f'./sample_hairstyle/sample_image/img{i+1:02d}.jpg').convert('RGBA')
+    # plt.imshow(img)
+    # plt.show()
 
-with open('./sample_hairstyle/sample_label.json') as f:
-    label = json.load(f)
+    with open('./sample_hairstyle/sample_label.json') as f:
+        label = json.load(f)
 
-coordinates = label['annotation'][0]['polygon1']
-coordinates = ast.literal_eval(coordinates)
+    coordinates = label['annotation'][i]['polygon1']
+    coordinates = ast.literal_eval(coordinates)
 
-for coor in coordinates:
-    x = coor['x']
-    y = coor['y']
-    print(x,y)
+    x = []
+    y = []
+    for coor in coordinates:
+        x.append(coor['x'])
+        y.append(coor['y'])
+
+    x = map(float, x)
+    y = map(float, y)
+    xy = list(zip(x, y))
+
+    img2 = img.copy()
+    draw = ImageDraw.Draw(img2)
+    draw.polygon(xy, fill = 'wheat')
+
+    img3 = Image.blend(img, img2, 0.5)
+    plt.imshow(img3)
+    plt.show()

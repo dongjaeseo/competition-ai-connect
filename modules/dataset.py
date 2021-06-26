bc0617827fb9359f8f5f65aa62e12804cb1a5821
DongJae Seo <75518803+dongjaeseo@users.noreply.github.com>
2021-06-26 16:53:05 +0900

    20210626 1653

--- modules/dataset.py
+++ modules/dataset.py
@@ -74,20 +74,16 @@ class Dataset_tu(BaseDataset):
         json_file = utils.load_json(os.path.join(path,'labels.json'))
 
         json_file['annotations'] = sorted(json_file['annotations'], key = lambda x: list(x.items())[0])
-        i =0
-        while True:
-            print(json_file['annotations'][i]['file_name'])
-            i+=1
 
-        # for i, file_path in enumerate(os.listdir(os.path.join(path,'images'))):
-        #     polygon = []
-        #     for line in json_file['annotations'][i]['polygon1']:
-        #         xy = list(line.values())
-        #         polygon.append((xy[0], xy[1]))
+        for i, file_path in enumerate(os.listdir(os.path.join(path,'images'))):
+            polygon = []
+            for line in json_file['annotations'][i]['polygon1']:
+                xy = list(line.values())
+                polygon.append((xy[0], xy[1]))
 
-        #     img = Image.new('L', (512, 512), 'black')
-        #     ImageDraw.Draw(img).polygon(polygon, outline='white', fill='white')
-        #     img.save(os.path.join(mask_dir,file_path.split('.')[0]+'.jpg'))
+            img = Image.new('L', (512, 512), 'black')
+            ImageDraw.Draw(img).polygon(polygon, outline='white', fill='white')
+            img.save(os.path.join(mask_dir,file_path.split('.')[0]+'.jpg'))
 
 
 if __name__=='__main__':
